dc5815a4875c7d3fc14ce2df3d98d4a3234a38a2
encode-abdullah <abdullaharif0789@gmail.com>
2026-06-17 18:52:25 +0500

    added user existence check in auth middleware after token verify

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -13,6 +13,12 @@ const protect = asyncHandler(async (req, res, next) => {
       token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select('-password');
+      
+      if (!req.user) {
+        res.status(401);
+        throw new Error('User not found');
+      }
+      
       next();
     } catch (error) {
       console.error(error);
