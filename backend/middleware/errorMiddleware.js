362a6bca95af9836ba1fab81e8f50c015d988662
encode-abdullah <abdullaharif0789@gmail.com>
2026-06-17 19:00:52 +0500

    improved error handler with casterror handling for invalid ObjectIds

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -6,11 +6,18 @@ const notFound = (req, res, next) => {
 
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
+  
+  const response = {
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-  });
+  };
+
+  if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    response.message = 'Resource not found';
+    res.status(404);
+  }
+
+  res.status(statusCode).json(response);
 };
 
 module.exports = { notFound, errorHandler };
